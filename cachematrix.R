54c5f5e9559f28f870aa0caa3797f2722c235323
unknown <snegi@CARS1401.corp.classifiedventures.com>
2015-09-29 13:11:27 -0500

    Committing my changes for assignment -2. I know its late

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCatchMatrix <- function(x = matrix()){
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             getinverse = getinverse,
+             setinverse = setinverse)
+        
 }
 
+## Writing a function to to return the inverse of the above matrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+catchSolve <- function(x, ...){
+        m <- x$getinverse()
+        if (!is.null(m)){
+                message("getting cashed data")
+                return(m)
+                
+        }
+        data <- x$get()
+        m <-solve(data, ...)
+        x$setinverse(m)
+        m
 }
